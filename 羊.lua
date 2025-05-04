0182ead4d6cfb775fcdfab3273a58364a43b91ad
鬼 <3065395172@qq.com>
2025-05-04 21:05:26 +0800

    Add files via upload

--- 羊.lua
+++ 羊.lua
@@ -28,7 +28,7 @@ function Ripple(obj)
 		Ripple.BackgroundColor3 = Color3.fromRGB(139, 0, 255)
 		Ripple.BackgroundTransparency = 1.000
 		Ripple.ZIndex = 8
-		Ripple.Image = "rbxassetid://18107430261"
+		Ripple.Image = "rbxassetid://78699805802392" --左边一栏
 		Ripple.ImageTransparency = 0.800
 		Ripple.ScaleType = Enum.ScaleType.Fit
 		Ripple.ImageColor3 = Color3.fromRGB(139, 0, 255)
@@ -121,10 +121,10 @@ function library.new(library, name,theme)
           v:Destroy()
         end
       end
--------------------------AL------------------------------
+-------------------------GHOST TEAM------------------------------
 GSTransparency = 0.6
 GScolor = Color3.fromRGB(0,255,0)
--------------------------AL------------------------------
+-------------------------GHOST TEAM------------------------------
 if theme == 'dark' then
     MainColor = Color3.fromRGB(25, 25, 25)
     Background = Color3.fromRGB(25, 25, 25)
@@ -216,7 +216,7 @@ end
       DropShadow.BackgroundTransparency = 1.000
       DropShadow.Position = UDim2.new(0.5, 0, 0.5, 0)
       DropShadow.Size = UDim2.new(1, 10, 1, 10)
-      DropShadow.Image = "rbxassetid://87437251671184" --背景
+      DropShadow.Image = "rbxassetid://87437251671184" -- 背景
       DropShadow.ImageColor3 = Color3.fromRGB(255,255,255)
       DropShadow.SliceCenter = Rect.new(49, 49, 450, 450)
 
@@ -431,7 +431,7 @@ Open.BorderSizePixel = 0
 Open.Size = UDim2.new(0, 50, 0, 50)
 Open.Active = true
 Open.Draggable = true
-Open.Image = "rbxassetid://18107430261"
+Open.Image = "rbxassetid://18107430261" -- 默认
 Open.MouseButton1Click:Connect(function()
   Main.Visible = not Main.Visible
   Open.Image = Main.Visible and "rbxassetid://128512006232061" or "rbxassetid://128512006232061" --开关的图
@@ -550,14 +550,14 @@ UIG.Parent = Open
           SectionOpen.BorderSizePixel = 0
           SectionOpen.Position = UDim2.new(0, -33, 0, 5)
           SectionOpen.Size = UDim2.new(0, 26, 0, 26)
-          SectionOpen.Image = "rbxassetid://6031302934"
+          SectionOpen.Image = "rbxassetid://6031302934" -- 右边的分区
           
           SectionOpened.Name = "SectionOpened"
           SectionOpened.Parent = SectionOpen
           SectionOpened.BackgroundTransparency = 1.000
           SectionOpened.BorderSizePixel = 0
           SectionOpened.Size = UDim2.new(0, 26, 0, 26)
-          SectionOpened.Image = "rbxassetid://6031302932"
+          SectionOpened.Image = "rbxassetid://6031302932" -- 右边的分区
           SectionOpened.ImageTransparency = 1.000
     
           SectionToggle.Name = "SectionToggle"
